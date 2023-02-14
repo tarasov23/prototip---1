6b0fb0038dd2e29bebf7551f58e3348e906461ed
Tarasov Konstantin <drossselmeier@gmail.com>
2023-02-14 17:09:12 +0500

    Функции

--- Operac.py
+++ Operac.py
@@ -1,9 +1,9 @@
 def roman_int(num):
     res = 0
-    d = {'CM':900, 'CD':400, 'XC': 90, 'XL': 40, 'IX': 9, 'IV':4,
+    cif = {'CM':900, 'CD':400, 'XC': 90, 'XL': 40, 'IX': 9, 'IV':4,
         'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-    for k in d:
-        res += d[k] * num.count(k)
+    for k in cif:
+        res += cif[k] * num.count(k)
         num = num.replace(k, '')
     return res
 
